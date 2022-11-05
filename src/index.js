f1a0314a6af9e29d8496871b4da4abcb0e11f4fd
Gabriel Haruki <gabrielharukisatoh@gmail.com>
2022-11-05 16:30:03 -0300

    feat: create global style

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
 import { App } from './pages'
 
-import './index.css'
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>

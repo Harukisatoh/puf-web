55587585e399fd7a101a0fa0dce36d765a6db42a
Gabriel Haruki <gabrielharukisatoh@gmail.com>
2022-11-06 14:39:55 -0300

    fix: allow margin or padding to use the first value of the scale (index 0)

--- src/components/Theme/styled.js
+++ src/components/Theme/styled.js
@@ -32,29 +32,29 @@ export const fontSize = props =>
   props.fontSize && `font-size: ${props.theme.fontSizes[props.fontSize]};`
 
 export const margin = props => {
-  const mt = props.mt || props.my || props.m || ''
-  const mr = props.mr || props.mx || props.m || ''
-  const mb = props.mb || props.my || props.m || ''
-  const ml = props.ml || props.mx || props.m || ''
+  const mt = props.mt ?? props.my ?? props.m ?? ''
+  const mr = props.mr ?? props.mx ?? props.m ?? ''
+  const mb = props.mb ?? props.my ?? props.m ?? ''
+  const ml = props.ml ?? props.mx ?? props.m ?? ''
 
   return `
-    ${mt && `margin-top: ${props.theme.spaces[mt]}px`};
-    ${mr && `margin-right: ${props.theme.spaces[mr]}px`};
-    ${mb && `margin-bottom: ${props.theme.spaces[mb]}px`};
-    ${ml && `margin-left: ${props.theme.spaces[ml]}px`};
+    ${mt !== '' ? `margin-top: ${props.theme.spaces[mt]}px;` : ''}
+    ${mr !== '' ? `margin-right: ${props.theme.spaces[mr]}px;` : ''}
+    ${mb !== '' ? `margin-bottom: ${props.theme.spaces[mb]}px;` : ''}
+    ${ml !== '' ? `margin-left: ${props.theme.spaces[ml]}px;` : ''}
   `
 }
 
 export const padding = props => {
-  const pt = props.pt || props.py || props.p || ''
-  const pr = props.pr || props.px || props.p || ''
-  const pb = props.pb || props.py || props.p || ''
-  const pl = props.pl || props.px || props.p || ''
+  const pt = props.pt ?? props.py ?? props.p ?? ''
+  const pr = props.pr ?? props.px ?? props.p ?? ''
+  const pb = props.pb ?? props.py ?? props.p ?? ''
+  const pl = props.pl ?? props.px ?? props.p ?? ''
 
   return `
-    ${pt && `padding-top: ${props.theme.spaces[pt]}px`};
-    ${pr && `padding-right: ${props.theme.spaces[pr]}px`};
-    ${pb && `padding-bottom: ${props.theme.spaces[pb]}px`};
-    ${pl && `padding-left: ${props.theme.spaces[pl]}px`};
+    ${pt !== '' ? `padding-top: ${props.theme.spaces[pt]}px;` : ''}
+    ${pr !== '' ? `padding-right: ${props.theme.spaces[pr]}px;` : ''}
+    ${pb !== '' ? `padding-bottom: ${props.theme.spaces[pb]}px;` : ''}
+    ${pl !== '' ? `padding-left: ${props.theme.spaces[pl]}px;` : ''}
   `
 }

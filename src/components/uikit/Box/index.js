620b494ac69921c73ad9d3c73b542afefe91f0a8
Gabriel Haruki <gabrielharukisatoh@gmail.com>
2022-11-06 15:00:57 -0300

    feat: use font helper inside box styled component

--- src/components/uikit/Box/index.js
+++ src/components/uikit/Box/index.js
@@ -1,10 +1,17 @@
 import styled from 'styled-components'
 
-import { background, margin, padding, flexbox } from '~/components/Theme/styled'
+import {
+  background,
+  margin,
+  padding,
+  flexbox,
+  font,
+} from '~/components/Theme/styled'
 
 export const Box = styled('div')`
   ${background}
   ${padding}
   ${margin}
   ${flexbox}
+  ${font}
 `

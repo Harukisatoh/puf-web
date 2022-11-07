2d5eb12f0a69abd2e72b4c22471658925e732c3d
Gabriel Haruki <gabrielharukisatoh@gmail.com>
2022-11-06 23:34:36 -0300

    refactor: extract form to another file and create centered box component

---
+++ src/pages/Signup/Form/index.js
@@ -0,0 +1,92 @@
+import * as React from 'react'
+import axios from 'axios'
+import * as yup from 'yup'
+import { useFormik } from 'formik'
+import styled from 'styled-components'
+
+import { Field, Box, Button, font, margin } from '~/components'
+
+const Link = styled('a')`
+  text-decoration: none;
+
+  ${font}
+  ${margin}
+`
+
+const validationSchema = yup.object().shape({
+  name: yup.string().required('Informe o seu nome'),
+  email: yup.string().required('Informe o seu e-mail').email('E-mail inválido'),
+  password: yup.string().required('Digite uma senha'),
+})
+
+export const Form = () => {
+  const onSubmit = async () => {
+    try {
+      await axios.post('http://localhost:9901/users', values)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const {
+    values,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    isSubmitting,
+    errors,
+    touched,
+  } = useFormik({
+    onSubmit,
+    validationSchema,
+    initialValues: { name: '', email: '', password: '' },
+  })
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Field
+        name="name"
+        label="Nome"
+        type="text"
+        value={values.name}
+        disabled={isSubmitting}
+        error={touched.name && errors.name}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        mb={3}
+      />
+      <Field
+        name="email"
+        label="Email"
+        type="text"
+        value={values.email}
+        disabled={isSubmitting}
+        error={touched.email && errors.email}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        mb={3}
+      />
+      <Field
+        name="password"
+        label="Senha"
+        type="password"
+        value={values.password}
+        disabled={isSubmitting}
+        error={touched.password && errors.password}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        mb={3}
+      />
+
+      <Box flexbox="column" center>
+        <Button type="submit" loading={isSubmitting} mb={1}>
+          Registrar
+        </Button>
+
+        <Link href="#" mt={1} fontSize={1} color="gray" fontWeight="bold">
+          Já sou cadastrado!
+        </Link>
+      </Box>
+    </form>
+  )
+}
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,102 +1,28 @@
 import * as React from 'react'
-import axios from 'axios'
-import * as yup from 'yup'
-import { useFormik } from 'formik'
 import styled from 'styled-components'
 
-import { Field, Box, Button, font, margin } from '~/components'
+import { Box, font } from '~/components'
+
+import { Form } from './Form'
 
 const Title = styled('h2')`
   ${font}
 `
 
-const Link = styled('a')`
-  text-decoration: none;
-
-  ${font}
-  ${margin}
-`
-
-const validationSchema = yup.object().shape({
-  name: yup.string().required('Informe o seu nome'),
-  email: yup.string().required('Informe o seu e-mail').email('E-mail inválido'),
-  password: yup.string().required('Digite uma senha'),
-})
+const CenteredBox = ({ children, ...props }) => (
+  <Box {...props} flexbox="column" flex={1} center>
+    <Box style={{ width: 445 }}>{children}</Box>
+  </Box>
+)
 
 export const Signup = () => {
-  const onSubmit = async () => {
-    try {
-      await axios.post('http://localhost:9901/users', values)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  const {
-    values,
-    handleChange,
-    handleBlur,
-    handleSubmit,
-    isSubmitting,
-    errors,
-    touched,
-  } = useFormik({
-    onSubmit,
-    validationSchema,
-    initialValues: { name: '', email: '', password: '' },
-  })
-
   return (
-    <Box as="main" flexbox="column" flex={1} center>
-      <Box style={{ width: 380 }}>
+    <Box flex={1} flexbox>
+      <CenteredBox bg="black">Esquerdo</CenteredBox>
+      <CenteredBox as="main">
         <Title textAlign="center">Cadastro</Title>
-
-        <form onSubmit={handleSubmit}>
-          <Field
-            name="name"
-            label="Nome"
-            type="text"
-            value={values.name}
-            disabled={isSubmitting}
-            error={touched.name && errors.name}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            mb={3}
-          />
-          <Field
-            name="email"
-            label="Email"
-            type="text"
-            value={values.email}
-            disabled={isSubmitting}
-            error={touched.email && errors.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            mb={3}
-          />
-          <Field
-            name="password"
-            label="Senha"
-            type="password"
-            value={values.password}
-            disabled={isSubmitting}
-            error={touched.password && errors.password}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            mb={3}
-          />
-
-          <Box flexbox="column" center>
-            <Button type="submit" loading={isSubmitting} mb={1}>
-              Registrar
-            </Button>
-
-            <Link href="#" mt={1} fontSize={1} color="gray" fontWeight="bold">
-              Já sou cadastrado!
-            </Link>
-          </Box>
-        </form>
-      </Box>
+        <Form />
+      </CenteredBox>
     </Box>
   )
 }
